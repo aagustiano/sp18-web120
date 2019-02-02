396c1b6f06dee08963b1bd3e8c08cc052e20052d
aagustiano <47265073+aagustiano@users.noreply.github.com>
2019-02-01 20:23:40 -0800

    Add files via upload

--- web120/includes/header.php
+++ web120/includes/header.php
@@ -21,6 +21,8 @@
   <h1><a href="index.php"><i class="logo fa <?=$logo?>"></i> Alexa's WEB120 Portal</a></h1>
   <nav>
     <ul class="topnav" id="myTopnav">
+        <?=makeLinks($nav1)?>
+    <!-- START COMMENT OUT NAV
       <li>&nbsp;<a href="index.php" class="<?= ($selectedPage == 'index') ? 'selected':''; ?>"><i class="fa fa-home" aria-hidden="true"></i> Home</a></li>
       <li><a href="big/index.php"><i class="fa fa-file-text" aria-hidden="true"></i> Big</a></li>
       <li><a href="aia.php" class="<?= ($selectedPage == 'aia') ? 'selected':''; ?>"><i class="fa fa-users" aria-hidden="true"></i> AIA</a></li>
@@ -28,6 +30,7 @@
       <li><a href="fp/index.php"><i class="fa fa-desktop" aria-hidden="true"></i> Final Project</a></li>
       <li><a href="contactme.php" class="<?= ($selectedPage == 'contactme') ? 'selected':''; ?>"><i class="fa fa-envelope" aria-hidden="true"></i> Contact Alexa</a></li>
       <li class="icon"> <a href="javascript:void(0);" onclick="myFunction()">&#9776;</a> </li>
+    END COMMENT-->
     </ul>
   </nav>
 </header>
